032b1b631da5f308f443c16ab7e5d41069e47019
Lerisson Freitas <lff3@cin.ufpe.br>
2017-11-29 02:38:34 +0000

    adição da pagina home no public

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'user_info/welcome'
+ # get 'user_info/welcome'
 
-  root :to => 'home#home'
-  get '/signup' => 'user#signup'
-  post '/welcome' => 'user_info#welcome'
+  #root :to => 'home#home'
+  #get '/signup' => 'user#signup'
+ # post '/welcome' => 'user_info#welcome'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
